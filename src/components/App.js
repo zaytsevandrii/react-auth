b4c992c17b5ae3c1bd98f08251617aa55e4f339e
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-23 17:15:24 +0000

    create context, components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap"
+import SignUp from "./SignUp"
+
+function App() {
+    return (
+        <Container className="d-flex align-item-center justify-content-center" style={{minHeight:'100vh'}}>
+          <div className="w-100" style={{maxWidth:'400px'}}>
+            <SignUp /></div>
+        </Container>
+    )
+}
+
+export default App
